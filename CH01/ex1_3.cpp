a067dd56b7af0de55371d19378edbe288bea62fe
徐意 <540767457@qq.com>
2017-06-16 10:21:20 +0800

    test for example.h

--- CH01/ex1_3.cpp
+++ CH01/ex1_3.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "test.h"
 
 int main()
 {
 	using namespace std;
 	cout<<"Hello, World"<<endl;
+	cout<<add(1,1)<<endl;
 	return 0;
 }
---
+++ CH01/test.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H_
+#define TEST_H_
+
+int add(int a, int b)
+{
+	return a+b;
+}
+
+#endif
