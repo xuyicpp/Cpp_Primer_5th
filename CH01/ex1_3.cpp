8ede6f2494fe71fd9f80a00d7d3707664a9c9861
徐意 <540767457@qq.com>
2017-06-16 10:47:33 +0800

    把测试多文件编译单独提出来了
    初学者具体参考这篇BLOG,http://blog.csdn.net/jk198310/article/details/16982735

--- CH01/ex1_3.cpp
+++ CH01/ex1_3.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include "test.h"
 
 int main()
 {
 	using namespace std;
 	cout<<"Hello, World"<<endl;
-	cout<<add(1,1)<<endl;
 	return 0;
 }
---
+++ CH01/test_compile_mul_file.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test_compile_mul_file"
+using namespace std;
+
+int main()
+{
+	cout<<"add(1,1)="<<add(1,1)<<endl;
+	return 0;
+}
