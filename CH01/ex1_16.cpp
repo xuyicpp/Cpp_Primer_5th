e66f1a6f83ae24fb90953db47b6b2e3218ac7b2c
小徐 <yixu_cs@hust.edu.cn>
2017-06-19 17:01:54 +0800

    add ex1_16

---
+++ CH01/ex1_16.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>		//for exit()
+
+int main(int argc, char *argv[])
+{
+	using namespace std;
+	if(argc==1)			//quit if no argument
+	{
+		cerr<<"Usage: "<<argv[0]<<" filename[s]\n";
+		exit(EXIT_FAILUERE);
+	}
+	ifstream fin;		//open stream
+	long count;
+	long total = 0;
+	int in;
+
+
+	for(int file=1;file<argc;file++)
+	{
+		fin.open(argv[file]);
+		count = 0;
+		while(fin>>in)
+		{
+			cout<<in<<endl;
+		}
+		cout<<count<<" int in "<<argv[file]<<endl;
+		total += in;
+		fin.clear();
+		fin.close();
+	}
+	cout<<"All int's value in all files is "
+	<<total<<endl;
+
+	return 0;
+}
