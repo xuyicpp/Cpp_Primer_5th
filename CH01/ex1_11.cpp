96f114bc0941263e0769bc9feb9acf1dda2fc1ce
小徐 <yixu_cs@hust.edu.cn>
2017-06-19 22:20:47 +0800

    add ex1_11.cpp

---
+++ CH01/ex1_11.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>		//for exit()
+
+int main(int argc, char *argv[])
+{
+	using namespace std;
+	if(argc==1)			//quit if no argument
+	{
+		cerr<<"Usage: "<<argv[0]<<" filename[s]\n";
+		exit(EXIT_FAILURE);
+	}
+	ifstream fin;		//open stream
+	long count;
+	long total = 0;
+	int min=30,max=0,temp=0;
+
+
+	for(int file=1;file<argc;file++)
+	{
+		fin.open(argv[file]);
+		while(fin>>in)
+		{
+			if(in>max)
+				max=in;
+			else if(in<min)
+				min=in;
+			
+		}
+		fin.clear();
+		fin.close();
+	}
+	for(int i=min+1;i<max;i++)
+		cout<<i<<" ";
+	cout<<endl;
+	return 0;
+}
