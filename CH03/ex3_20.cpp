2a721e7a37d7ff504333466ed363076d60df0def
徐意 <540767457@qq.com>
2017-06-26 11:01:54 +0800

    add ex3_20.cpp
    输出第一个和最后一个元素的和，然后是倒数2和正2，以此类推。

---
+++ CH03/ex3_20.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if(argc==1)			//quit if no argument
+	{
+		cerr<<"Usage: "<<argv[0]<<" filename[s]\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	int i;
+	vector<int> vi;
+
+	for(int file=1;file<argc;file++)
+	{
+		ifstream fin;		//open stream
+		fin.open(argv[file]);
+
+		while(fin>>i)
+			vi.push_back(i);
+
+		fin.clear();
+		fin.close();
+	}
+
+	for(decltype(vi.size()) begin=0,end=vi.size()-1;
+		begin>end;begin++,end--)
+		cout<<(vi[begin]+vi[end])<<" ";
+
+	return 0;
+}
