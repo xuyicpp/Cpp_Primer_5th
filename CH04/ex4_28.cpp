c3839924e2a9916d0d59bf09dbb574425f501611
徐意 <540767457@qq.com>
2017-06-29 20:30:12 +0800

    add CH04

---
+++ CH04/ex4_28.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"The size of int:"<<sizeof(int)<<" "
+	cout<<"The size of short:"<<sizeof(short)<<" "
+	cout<<"The size of long:"<<sizeof(long)<<" "<<endl;
+	cout<<"The size of unsigned int:"<<sizeof(unsigned int)<<endl;
+	return 0;
+}
