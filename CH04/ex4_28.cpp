6bea293358bc81277639f3d84fba8f2a88057251
徐意 <540767457@qq.com>
2017-06-29 20:45:56 +0800

    刚才travis罢工了，好神奇

--- CH04/ex4_28.cpp
+++ CH04/ex4_28.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
 	cout<<"The size of int:"<<sizeof(int)<<" "
-	cout<<"The size of short:"<<sizeof(short)<<" "
-	cout<<"The size of long:"<<sizeof(long)<<" "<<endl;
+	<<"The size of short:"<<sizeof(short)<<" "
+	<<"The size of long:"<<sizeof(long)<<" "<<endl;
 	cout<<"The size of unsigned int:"<<sizeof(unsigned int)<<endl;
 	return 0;
 }
