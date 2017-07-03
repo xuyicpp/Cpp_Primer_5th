03e148dad733d1b83a334c8ceabdbcf32db37411
徐意 <540767457@qq.com>
2017-07-03 16:41:27 +0800

    add ex5_21.cpp

---
+++ CH05/ex5_21.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>		//for exit()
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	if(argc==1)			//quit if no argument
+	{
+		cerr<<"Usage: "<<argv[0]<<" filename[s]\n";
+		exit(EXIT_FAILURE);
+	}
+
+	string temp,lastOne;
+
+	for(int file=1;file<argc;file++)
+	{
+		ifstream fin;		//open stream
+		fin.open(argv[file]);
+
+		fin>>lastOne;
+		while(fin>>temp)
+		{
+			if(!isupper(temp[0]))
+				continue;
+			else if(temp==lastOne)
+				break;
+			else
+				lastOne=temp;
+		}
+
+		fin.clear();
+		fin.close();
+	}
+
+	if(temp==lastOne)
+		cout<<"the same word is :"<<lastOne;
+	else
+		cout<<"no words repeat twice.";
+
+	return 0;
+}
