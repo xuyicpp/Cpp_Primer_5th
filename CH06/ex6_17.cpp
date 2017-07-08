b2da85a6420848c3efe680b6d65015362f6835b8
小徐 <yixu_cs@hust.edu.cn>
2017-07-08 21:31:48 +0800

    add ex6_17.cpp

--- CH06/ex6_17.cpp
+++ CH06/ex6_17.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 #include <cctype>
+#include <string>
 #include "ex6.h"
 
 using namespace std;
 
 int main()
 {
-
-
+	string s{"AAA"};
+	cout<<"Original string:"<<s<<"hasUP? "<<hasUP(s);
+	downString(s);
+	cout<<" tolower :"<<s;
 	return 0;
 }
 
 bool hasUP(const string &s)
 {
-
-	return true;
+	for(auto a: s )
+	{
+		if(isupper(a))
+			return true;
+	}
+	return false;
 }
 
 void downString(string &s)
 {
-
+	for(auto a: s)
+	{
+		a=tolower(a);
+	}
 	return;
 }
