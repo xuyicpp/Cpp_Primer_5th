fdda64d56b6c8f6fa8d61164b6e3c57f38a0c4cd
徐意 <540767457@qq.com>
2017-07-13 18:49:13 +0800

    add ex7_43.cpp

---
+++ CH07/ex7_43.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "ex7_43.h"
+
+using namespace std;
+
+int main()
+{
+	C c;	使用了类型C的默认构造函数
+	cout << c.nd.val << endl;
+	return 0;
+}
--- CH07/ex7_43.h
+++ CH07/ex7_43.h
@@ -1,5 +1,23 @@
 #ifndef EX7_43_H_
 #define EX7_43_H_
 
-class 
+//该类型没有显式定义默认构造函数，编译器也不会为它合成一个
+class NoDefault
+{
+public:
+	NoDefault(int i)
+	{
+		val = i;
+	}
+	int val;
+};
+
+class C
+{
+public:
+	NoDefault nd;
+	//必须显式调用Nodefault的带参构造函数初始化nd
+	C(int i = 0) : nd(i) { }
+};
+
 #endif
